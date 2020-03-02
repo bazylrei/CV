a13fee9253b8c7dbe8629f0a49422c9ffef9b05b
Bazyl Reinstein <bazylrei@gmail.com>
2020-03-02 15:45:25 +0100

    adds xcode project to repo

---
+++ CV/CV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CV
+//
+//  Created by Bazyl Reinstein on 2/3/20.
+//  Copyright © 2020 Bazyl Reinstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
