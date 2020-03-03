1c5b14153c127853898cc04f9ee01401cc203619
Bazyl Reinstein <bazylrei@gmail.com>
2020-03-03 11:36:57 +0100

    adds CVAPI facade

---
+++ CV/CV/Modules/CVAPI.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+protocol CVAPIType {
+    func fetchCVs()
+    func fetchCV(with identifier: String)
+}
+
+class CVAPI: NSObject {
+    func fetchCVs() {
+        
+    }
+    func fetchCV(with identifier: String) {
+        
+    }
+}
