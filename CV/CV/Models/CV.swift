7ca21a2b0d83f3e3d9918c7202e3a76e9836d3e3
Bazyl Reinstein <bazylrei@gmail.com>
2020-03-03 13:46:27 +0100

    adds courses entry to CV

--- CV/CV/Models/CV.swift
+++ CV/CV/Models/CV.swift
@@ -3,7 +3,7 @@ import Foundation
 class CV: NSObject, Codable {
     let person: Person
     let workEntries: [WorkEntry]?
-//    let courses: [Course]
+    let courses: [Course]
 //    let training: [Training]
 //    let skills: [Skill]
 }
