2346ab81cff9ccafcb6387f3503f35184ae2a70e
Bazyl Reinstein <bazylrei@gmail.com>
2020-03-03 22:08:13 +0100

    adds sections to CV details view

--- CV/CV/ViewModels/CVDetailsViewModel.swift
+++ CV/CV/ViewModels/CVDetailsViewModel.swift
@@ -1,8 +1,30 @@
 import Foundation
 
+enum Section: Int, CaseIterable {
+    case workEntries
+    case courses
+    case trainings
+    case skills
+    
+    var title: String {
+        switch self {
+        case .workEntries:
+            return "Previous positions"
+        case .courses:
+            return "Completed courses"
+        case .trainings:
+            return "Trainings"
+        case .skills:
+            return "Skills"
+        }
+    }
+}
+
 protocol CVDetailsViewModelType {
     var cv: CV? { get }
     var title: String { get }
+    var numberOfSections: Int { get }
+    func title(for section: Int) -> String?
 }
 
 class CVDetailsViewModel: CVDetailsViewModelType {
@@ -17,5 +39,16 @@ class CVDetailsViewModel: CVDetailsViewModelType {
             return ""
         }
         return "\(cv.person.firstName) \(cv.person.lastName)"
+    }
+    
+    lazy var numberOfSections: Int = {
+        guard let cv = cv else {
+            return 0
+        }
+        return Section.allCases.count
+    }()
+    
+    func title(for section: Int) -> String? {
+        return Section(rawValue: section)?.title
     }
 }
--- CV/CV/Views/CVDetailsViewController.swift
+++ CV/CV/Views/CVDetailsViewController.swift
@@ -2,10 +2,39 @@ import UIKit
 
 class CVDetailsViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     var viewModel: CVDetailsViewModelType?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         self.title = viewModel?.title
     }
+}
+
+extension CVDetailsViewController: UITableViewDelegate {
+    
+}
+
+extension CVDetailsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        guard let viewModel = viewModel else {
+            return 0
+        }
+        return viewModel.numberOfSections
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel?.title(for: section)
+    }
+    
 }
