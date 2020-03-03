d242092418ba94e27a380519f3ce8a000bf445fe
Bazyl Reinstein <bazylrei@gmail.com>
2020-03-03 13:43:28 +0100

    adds work entries to CV

--- CV/CV/Models/CV.swift
+++ CV/CV/Models/CV.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class CV: NSObject, Codable {
     let person: Person
-//    let workEntries: [WorkEntry]
+    let workEntries: [WorkEntry]?
 //    let courses: [Course]
 //    let training: [Training]
 //    let skills: [Skill]
--- CV/CV/Models/WorkEntry.swift
+++ CV/CV/Models/WorkEntry.swift
@@ -4,7 +4,7 @@ class WorkEntry: NSObject, Codable {
     let companyName: String
     let position: String
     let responsibilities: [String]?
-    let shortDescription: String
+    let shortDescription: String?
     let startDate: Date
     let endDate: Date?
 }
